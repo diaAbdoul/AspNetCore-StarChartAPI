9c7dd5e497a82e6c71b8c4be823b01bbacf31205
Dia Abdoul <dia.abdoul@gmail.com>
2019-11-25 15:43:41 -0500

    Update ApplicationDbContxt
    fix dbcontextoptions

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -12,8 +12,8 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options=>options.UseInMemoryDatabase("StartChart"));
-            services.AddMvc();
+                       services.AddMvc();
+                     services.AddDbContext<ApplicationDbContext>(options=>options.UseInMemoryDatabase("StartChart"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
